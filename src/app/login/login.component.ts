34fcada39276aa2f67d7dc9cb7d463d7d370f182
LeQuoc-Dat <lequocdat.it@gmail.com>
2026-02-24 13:01:12 +0700

    update-get-input-value

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,13 +1,24 @@
 import {Component } from '@angular/core';
+import {NgForm,  FormsModule} from '@angular/forms'
 
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
 export class Login {
-  username =''
-  password =''
+  onLogin(f: NgForm):void
+  {
+    if (f.valid)
+    {
+      alert(`Logged`)
+    }
+    else
+    {
+      alert('Fill username and password before submit')
+    }
+    
+  }
 }

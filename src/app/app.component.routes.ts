18c72133161a707c5996201a68175be40dc494fa
LeQuoc-Dat <lequocdat.it@gmail.com>
2026-02-25 09:16:18 +0700

    update-default-route

--- src/app/app.component.routes.ts
+++ src/app/app.component.routes.ts
@@ -5,6 +5,12 @@ import {LoginComponent} from './login/login.component'
 
 export const routes: Routes =[
     {
+        path:'', 
+        redirectTo: 'login',
+        pathMatch:'full'
+    }
+    ,
+    {
         path:'login',
         component: LoginComponent,
         title:'Login Page'
